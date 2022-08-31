a62fe33a761e494faa18b6e74211ccb140fe8de9
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2022-08-30 21:06:20 -0300

    .

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plantree/widgets/home_page.dart';
+import 'package:plantree/widgets/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:plantree/main.dart';
+import 'package:plantree/widgets/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
